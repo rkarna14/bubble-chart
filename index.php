168c1d1dbfc50e9f6849754f70562c4c620323b4
rkarna14 <karna.rupak14@gmail.com>
2016-10-22 10:43:27 +0545

    added index.php for detection of build choice for heroku

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>HTML Seed</title>
+  <meta name="keywords" content="HTML, Seed Project">
+  <meta name="description" content="Seed Project for html5">
+  <meta name="author" content="Rupak">
+
+  <link rel="stylesheet" href="resources/css/bubbleChart.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <div id="bubble-chart-container" style="width:800px; height:600px;"></div>
+  
+  <script src="resources/libs/jquery-2.1.4.js"></script>
+  <script src="resources/libs/d3.min.js"></script>
+  <script src="resources/libs/d3.legend.js"></script>
+  <script src="resources/libs/d3.tip.v0.6.3.js"></script>
+  <script src="resources/js/bubbleChart.js"></script>
+</body>
+</html>
